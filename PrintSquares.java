8a43382fabb5f7ba9bb69f166b303877623af067
JasonE214 <khc214@exeter.ac.uk>
2026-03-25 18:18:26 +0000

    PrintSquares

---
+++ PrintSquares.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; // We use the Scanner class from java.util to handle user input
+import java.util.ArrayList;
+
+public class PrintSquares {
+    public static void main(String[] args) {
+
+        // Declare Scanner object
+        Scanner in = new Scanner(System.in);
+
+        // Prompt for input
+        System.out.print("Enter your name: ");
+
+        // Read in line from standard input
+        String name = in.nextLine();
+
+        System.out.println("Hi " + name + "!");
+
+
+        System.out.print("Please enter the start value: ");
+        int startNum = in.nextInt(); // Reads in an int from standard input
+        
+        System.out.print("Please enter the end value: ");
+        int endNum = in.nextInt();
+
+        ArrayList<Integer> square = new ArrayList<>();
+        for (int i = startNum; i <= endNum; i++) {
+            square.add(i * i);
+        }
+
+        System.out.println("You entered: " + startNum + " and " + endNum);
+        System.out.println("The square of the numbers you enter: " + square);
+
+    }
+}
